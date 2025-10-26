bd44624b64910a6b60567bdae17a0469b83dd9bf
chaeyoungeee <chaeinge@gmail.com>
2025-10-26 14:10:09 +0900

    refactor(ErrorMessage): 에러 메시지 수정

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     CAR_NAME_TOO_LONG("자동차 이름 최대 길이를 초과했습니다."),
     CAR_NAME_DUPLICATE("중복된 자동차 이름이 존재합니다."),
     TRY_COUNT_INVALID("시도 횟수가 정수가 아닙니다."),
-    TRY_COUNT_NEGATIVE("시도 횟수가 양의 정수가 아닙니다.");
+    TRY_COUNT_NEGATIVE("시도 횟수가 0 혹은 양의 정수가 아닙니다.");
 
     private final String message;
 
