506df6e6a428c1a8e76adf212c833418e06e0257
chaeyoungeee <chaeinge@gmail.com>
2025-10-26 22:04:49 +0900

    refactor(RandomNumberGenerator): 랜덤 숫자 생성기 인터페이스로 변경

---
+++ src/main/java/racingcar/utils/DefaultRandomNumberGenerator.java
@@ -0,0 +1,15 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class DefaultRandomNumberGenerator implements RandomNumberGenerator {
+
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
+    @Override
+    public int generate() {
+
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
+}
