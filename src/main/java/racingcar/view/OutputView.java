32c907859e45646adfae4015970405d319c934e8
chaeyoungeee <chaeinge@gmail.com>
2025-10-26 14:41:47 +0900

    feat(OutputView): 차수별 실행 결과 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
 
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String RESULT_OUTPUT_MESSAGE = "실행 결과";
 
     public void printCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -14,4 +15,21 @@ public class OutputView {
 
     public void printTryCountInputMessage() {
         System.out.println(TRY_COUNT_INPUT_MESSAGE);
+    }
+
+    public void printResultOutputMessage() {
+        System.out.println(RESULT_OUTPUT_MESSAGE);
+    }
+
+    public void println() {
+        System.out.println();
+    }
+
+    public void printResult(List<CarStatus> result) {
+        result.forEach(
+                status -> {
+                    System.out.println(status.getCarName() + " : " + "-".repeat(status.getForwardCount()));
+                }
+        );
+    }
 }
